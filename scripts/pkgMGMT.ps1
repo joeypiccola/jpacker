515d57c70ad2c71fb389b170466ae710ef7ce331
Joey Piccola <joey@joeypiccola.com>
2016-09-10 11:42:30 -0600

    edit 2

--- scripts/pkgMGMT.ps1
+++ scripts/pkgMGMT.ps1
@@ -1,4 +1,4 @@
-# for when wmf5 just isn't your thing.
+# for when wmf5 just isn't your thing...
 msiexec /qb /i a:\PackageManagement_x64.msi
 sleep -seconds 10
 Install-PackageProvider -Name NuGet -Force
