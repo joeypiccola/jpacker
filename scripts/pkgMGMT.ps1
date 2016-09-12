133b75d6b9741fc835fd239cb1d499667db0084d
Joey Piccola <joey@joeypiccola.com>
2016-09-11 21:25:11 -0600

    bin notes added to pkgMGMT

--- scripts/pkgMGMT.ps1
+++ scripts/pkgMGMT.ps1
@@ -1,4 +1,5 @@
 # for when wmf5 just isn't your thing...
+# assuming bin has been downloaded and sync'd up to a:\
 msiexec /qb /i a:\PackageManagement_x64.msi
 sleep -seconds 10
 Install-PackageProvider -Name NuGet -Force
