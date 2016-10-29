e3d50ee367938489e2534df60e0a18be9b472543
Joey Piccola <joey@joeypiccola.com>
2016-10-28 23:55:33 -0600

    added rdp support

---
+++ scripts/Enable-RDP.ps1
@@ -0,0 +1,2 @@
+(Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1,1) | Out-Null
+(Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0) | Out-Null
